0a132081e22ab6cfdbf23fcd17a6d34b77818694
Alexander Hurtado <alexanderjhurtado@gmail.com>
2021-03-03 17:07:04 -0800

    Add Firebase config

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAE0XqY7UIt84jbvDgwR_bWWkRy4k-X1EM",
+  authDomain: "cs-210-project.firebaseapp.com",
+  projectId: "cs-210-project",
+  storageBucket: "cs-210-project.appspot.com",
+  messagingSenderId: "263845644775",
+  appId: "1:263845644775:web:608290748a570d669d1542",
+  measurementId: "G-FSFDGW8XDY"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var firestore = firebase.firestore();
+
+export default firestore; 
