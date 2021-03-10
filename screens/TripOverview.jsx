8ce27909d1f4ab471ce706f0e22e373cb8b1fa7e
Anna Boonyanit <annaboon@stanford.edu>
2021-03-10 09:53:53 -0800

    Add title features and fix child component bug

--- screens/TripOverview.jsx
+++ screens/TripOverview.jsx
@@ -5,10 +5,15 @@ import {
   Text,
   TouchableOpacity,
   View,
+  TextInput,
 } from "react-native";
 import React, { useEffect, useState } from "react";
+import db from "../firebase";
 
 export default function TripOverview({ navigation, route }) {
+  const [tripTitle, setTripTitle] = useState("");
+  console.log("route", route.params);
+
   const pastTripComponent = ({ item }) => {
     return (
       <TouchableOpacity
@@ -29,13 +34,44 @@ export default function TripOverview({ navigation, route }) {
     );
   };
 
+  const onSaveTrip = async () => {
+    const pins = route.params["pins"];
+    var tripTitleText = tripTitle['text'];
+    if (tripTitleText == null) {
+      // Currently using a default name of road trip if user doesn't enter name
+      tripTitleText = "Road Trip";
+    }
+    const tripData = { tripTitleText, pins }; 
+    const collRef = db.collection("trips");
+    const newTripRef = await collRef.add(tripData);
+    console.log(`Added trip to Firebase reference: ${newTripRef.id}`);
+    navigation.navigate("Home");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Trip Overview</Text>
+      <View style={styles.tripTitleView}>
+        <Text style={styles.tripTitleText}> Trip title </Text>
+        <TextInput style={styles.titleInput}
+          placeholder="Enter trip title"
+          onChangeText={(text) => setTripTitle({text})}
+          defaultValue={route.params["tripTitleText"]}
+        />
+      </View>
       <Text style={styles.title}>Stops along the way:</Text>
-      {route.params["trip0"].length > 0 && (
-        <FlatList data={route.params["trip0"]} renderItem={pastTripComponent} />
+      {route.params["pins"].length > 0 && (
+        <FlatList 
+          data={route.params["pins"]} 
+          renderItem={pastTripComponent}
+          keyExtractor={(item, index) => index.toString()}
+          />
       )}
+      <TouchableOpacity
+        onPress={onSaveTrip}
+        style={styles.appButtonContainer}>
+        <Text style={styles.appButtonText}>Save Trip</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -75,4 +111,37 @@ const styles = StyleSheet.create({
     borderColor: "#00A398",
     borderWidth: 5,
   },
+ tripTitleView: {
+  margin: 10,
+  alignItems: 'stretch',
+  height: 40,
+  flexDirection:'row'
+ },
+ tripTitleText: {
+   marginTop: 10,
+   marginRight: 10,
+   fontWeight: "bold", 
+ },
+ titleInput: {
+  borderWidth: 1,
+  flex: 2,
+  borderColor: '#00A398',
+  padding: 5
+ },
+ appButtonContainer: {
+  elevation: 8,
+  backgroundColor: "#00A398",
+  borderRadius: 10,
+  paddingVertical: 10,
+  paddingHorizontal: 12,
+  marginVertical: 15
+},
+appButtonText: {
+  fontSize: 18,
+  color: "#fff",
+  fontWeight: "bold",
+  alignSelf: "center",
+  textTransform: "uppercase",
+},
+ 
 });
